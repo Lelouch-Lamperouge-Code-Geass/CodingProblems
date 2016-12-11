eddbde1b9782421ce96dddbab25c3df8c7302de1
Yang Liu <yangliu1989@nyu.edu>
2016-12-11 14:06:42 -0500

    Update longest_chain.cpp

--- longest_chain.cpp
+++ longest_chain.cpp
@@ -4,39 +4,47 @@ longest chain
 那么就形成一个 chain： old word -> new word -> newer word, 求最长长度(return int) 
 比如给vector<string> w = {a,ba,bca,bda,bdca} 最长是4： bdca->bda->ba->a;
 **/
-int LongestChainLength(const string & word,unordered_map<string,int> & longest_chain_len,unordered_set<string> & dict){
-	if (longest_chain_len.find(word) !=longest_chain_len.end()) return longest_chain_len[word];
-	const int word_len = word.size();
-	int res(1);// default the chain only contains itself
-	for (int i=0;i<word_len;++i) {
-		string temp = word.substr(0,i)+word.substr(i+1); // delete one char
-		if (dict.find(temp)!=dict.end()) { // sub-word exists
-			res = std::max(res, LongestChainLength(temp,longest_chain_len,dict) + 1);
+// Calculate the longest chain length for a specific word and return it.
+int LongestChainLen(const std::string & word,
+		std::unordered_map<std::string, int> & chain_len,
+		const std::unordered_set<std::string> & word_set) {
+	if (chain_len.find(word)!=chain_len.end()) return chain_len[word];
+	const int word_size(word.size());
+	int max_len(1);// For each word, its chain len is at least 1
+	for (int i=0;i<word_size;++i) {
+		const std::string & mut_word = word.substr(0,i)+word.substr(i+1);// try cut off every char
+		if ( word_set.find(mut_word)!=word_set.end() ) {
+			max_len = std::max(max_len,1+LongestChainLen(mut_word,chain_len,word_set));
 		}
 	}
-	longest_chain_len[word] = res;
-	return res;
+	chain_len[word] = max_len;
+	return max_len;
 }
 
 int FindLongestChain(vector<string> & words) {
-	unordered_map<string,int> longest_chain_len; // word and its relative longest chain, avoid duplicate calculation
-	unordered_set<string> dict;
-	for (const string & word : words) {
-		dict.insert(word);
-	}
-
-	int reval(0);
-
-	for (string & word : words) {
-		int len = LongestChainLength(word,longest_chain_len,dict);
-		reval = std::max(reval,len);
+	std::unordered_map<std::string, int> chain_len;
+	std::unordered_set<std::string> words_set(words.begin(),words.end());
+	int reval(0); // words could be empty
+	for (const std::string & word : words_set) {
+		reval = std::max(reval,LongestChainLen(word,chain_len,words_set));
 	}
 	return reval;
 }
 
-int main(){
+
+int _tmain(int argc, _TCHAR* argv[])
+{
 	string arr[] = {"a","abcd","bcd","abd","cd","c"};
 	vector<string> vec(std::begin(arr),std::end(arr));
 	int val = FindLongestChain(vec);
+	assert(val==4);
+	
+
+	string arr2[]  = {"abcde","abc","ab","efabc","fabc","abc","ac","abcd","c"};
+	vector<string> vec2(std::begin(arr2),std::end(arr2));
+	int val2 = FindLongestChain(vec2);
+	assert(val2==5);
 	return 0;
 }
+
+
