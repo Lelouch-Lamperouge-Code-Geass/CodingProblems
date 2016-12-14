e5ecdf1dfd3128fa6fef16accab02b3cb0197f83
Augustus Pope <yl916@nyu.edu>
2016-12-13 22:10:36 -0500

    rename

--- reverse_polish_notation_ood_unit_tests.cpp
+++
@@ -1,9 +0,0 @@
-class TokenizerTest : public CppUnit::TestCase {
-public:
-  TokenizerTest( std::string name ) : CppUnit::TestCase( name ) {}
-
-  void runTest() {
-    CPPUNIT_ASSERT( Complex (10, 1) == Complex (10, 1) );
-    CPPUNIT_ASSERT( !(Complex (1, 1) == Complex (2, 2)) );
-  }
-};
