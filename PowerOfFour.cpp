e6e889228aa3b12809f71e4cdad983b9b245910d
Augustus Pope <yl916@nyu.edu>
2017-01-14 12:39:58 -0500

    update power of Four

--- PowerOfFour.cpp
+++ PowerOfFour.cpp
@@ -1,20 +1,62 @@
-// Check whether a long number is power of 4 or not. Time complexity and space complexity should all be constant.
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <climits>
 
-bool PowerOfFour(long number) {
-  if (number<=0) return false; // Maker sure not negative or zero
-  if ( number & (number-1) !=0 ) return false; // Make sure there is only one bit set
-  return (0xAAAAAAAA & number) == 0 ; // 'A' bit form is 1010 <3 <3 <3
+bool PowerOfFour(long num) {
+  if (num<0) return false;
+  // Check whether it only has one setting bit
+  if ( (num & (num-1)) != 0 ) return false;
+
+  // The setting bit must show up on specific position.
+  // Notice in C++, the long is usualy 32 bit.
+  if ( (num & 0xAAAAAAAA) != 0 )return false;
+  // Or you can use (num & 0x55555555) != 0 here, basically the same
+  
+  return true;
 }
 
-
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-	std::cout << (PowerOfFour(1) ? "TRUE" : "FALSE") << std::endl;
-	std::cout << (PowerOfFour(2) ? "TRUE" : "FALSE") << std::endl;
-	std::cout << (PowerOfFour(3) ? "TRUE" : "FALSE") << std::endl;
-	std::cout << (PowerOfFour(4) ? "TRUE" : "FALSE") << std::endl;
-	return 0;
+void UnitTest_PowerOfFour() {
+  assert(PowerOfFour(1) == true);
+  assert(PowerOfFour(4) == true);
+  assert(PowerOfFour(16) == true);
+  assert(PowerOfFour(64) == true);
+  assert(PowerOfFour(256) == true);
+  assert(PowerOfFour(2) == false);
+  assert(PowerOfFour(3) == false);
+  assert(PowerOfFour(5) == false);
+  assert(PowerOfFour(6) == false);
+  assert(PowerOfFour(7) == false);
+  assert(PowerOfFour(35) == false);
 }
 
+// Bit manipulation doesn't work here anymore.
+// Instead, we find the maximum number that is
+// power of three, and check whether it is a multiple
+// of the input. This method works on other prime number as well.
+bool PowerOfThree(int num) {
+  if (num<=0) return false;
+  static int max_power_of_three = std::pow(3, (int)(std::log(INT_MAX) / std::log(3)) );
+  return max_power_of_three % num == 0;
+}
 
+void UnitTest_PowerOfThree() {
+  assert(PowerOfThree(1) == true);
+  assert(PowerOfThree(3) == true);
+  assert(PowerOfThree(27) == true);
+  assert(PowerOfThree(1162261467) == true);
+  assert(PowerOfThree(81) == true);
+  
+  assert(PowerOfThree(2) == false);
+  assert(PowerOfThree(4) == false);
+  assert(PowerOfThree(256) == false);
+  assert(PowerOfThree(6) == false);
+  assert(PowerOfThree(7) == false);
+  assert(PowerOfThree(35) == false);
+}
+
+int main() {
+  UnitTest_PowerOfFour();
+  UnitTest_PowerOfThree();
+  return 0;
+}
