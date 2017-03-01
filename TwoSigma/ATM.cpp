5dd4c48707b246092f13152cd6ba6372682c702e
Augustus Pope <yl916@nyu.edu>
2017-02-28 21:24:17 -0500

    update ATM OOD

--- TwoSigma/ATM.cpp
+++ TwoSigma/ATM.cpp
@@ -22,48 +22,78 @@ class ATMCard {
 private:
   std::string m_card_number;
 };
-typedef std::shared_ptr<ATMCard> ATMCardPtr;
 
+// ATM state interface
+class ATMState {
+public:
+  void InsertCard() = 0;
+  void VerifyPin() = 0;
+  void Deposit() = 0;
+  void Withdraw() = 0;
+  void Inquery() = 0;
+};
+
+// Concrete state classes, they should be singleton.
+// They won't have any member variables in general, so they are thread-safe
+class NoCardState : public ATMState { }; // this is the first state
+class HasCardState : public ATMState { }; // has card, not verify PIN yet
+class VerifiedPinState : public ATMState { }; // verified PIN
+
+// maintain reference to ATMState
 class ATM {
 public:
   ATM(){}
-  void InsertCard(ATMCardPtr & card_ptr) {
-    
-  }
-  void VerifyPin(const std::string & pin) {
-    
-  }
+  void InsertCard() {}
+  void VerifyPin() {}
   void Deposit() {}
   void Withdraw() {}
   void Inquery() {}
-
-};
-class User {
-  bool VerifyPin(const std::string & pin) {
-    return  pin == m_pin_number;
-  }
 private:
-  std::string m_name;
-  std::string m_pin_number; 
+  void ChangeState(ATMStatePtr & state){m_state_ptr = state;}
+  ATMStatePtr m_state_ptr; // maintain 
 };
 
+class User { };
 
-enum AccountType {
-  CHECKING = 0,
-  SAVING
-};
-
+// Account abstract class.
+// General implementation should be defined here.
+// While leave derived classes for account-type specific implementation.
 class Account {
 public:
-  void Deposit(double val, const AccountType & account_type) {
-    if (account_type == CHECKING) {
-      m_checking_balance += val;
-    } else {
-      m_saving_balance += val;
-    }
-  }
-  void Withdraw(double val) {}
-private:
-  double m_checking_balance;
-  double m_saving_balance;
+  void Deposit() {}
+  void Withdraw() {}
+  void Inquiry() {}
+  void Transfer() {}
 };
+
+class CheckingAccount : public Account {};
+class SavingAccount : public Account {};
+
+//Command pattern, memonto pattern may be suitable here depends on requirements.
+class Transaction {
+public:
+  virtual Execute() = 0;
+  virtual void Undo() = 0; 
+  virtual void Redo() = 0; 
+};
+
+class Withdraw : public Transaction {
+public:
+  Withdraw(AccountPtr account_ptr) {}
+};
+
+// same for other transactions
+class Deposit : public Transaction {};
+class Inquiry : public Transaction {};
+class Transfer : public Transaction {};
+
+// Offer limited interaction with relational database
+class BankDatabaseProxy {
+public:
+  UserPtr AuthenticateUser(const std::string & card_number, const std::string & pin);
+  void Transfer(AccountPtr account, double amount);
+  void Inquiry(AccountPtr account);
+  void Deposit(AccountPtr account, double amount);
+  void Withdraw(AccountPtr account, double amount);
+};
+
