ac0f54a3ef9e4b0067fe6dfded8af4d540240f1c
Yang Liu <yangliu1989@nyu.edu>
2017-03-04 13:16:32 -0500

    Update ModFiveIterator.cpp

--- TwoSigma/ModFiveIterator.cpp
+++ TwoSigma/ModFiveIterator.cpp
@@ -1,85 +1,103 @@
-// Given an Iterator class of a random number collection, write a new Iterator class
-// which only iterates numbers who mods five ==0. For example,5,25,15,90.
+#include <list>
+#include <iostream>
+#include <cassert>
 
-class Iterator {
+
+
+class ModFiveIterator {
 public:
-	Iterator(const vector<int>& nums):
-	m_nums(nums),
-	m_nums_size(nums.size()),
-	m_pos(0){
+  typedef std::list<int>::iterator ListIterator;
+  ModFiveIterator(const ListIterator & begin, const ListIterator & end)
+    : m_pos(begin), m_end(end), m_peeked(false) {
+  }
 
-	}
-    Iterator(const Iterator& iter);
-	virtual ~Iterator(){}
-    	// Returns the next element in the iteration.
-	int next(){
-		return m_nums[m_pos++];
-	}
-    	// Returns true if the iteration has more elements.
-	bool hasNext(){
-		return m_pos < m_nums_size;
-	}
-	void remove(){}
+  bool HasNext() {
+    TryPeek();
+    return m_peeked;
+  }
+
+  int Next() {
+    TryPeek();
+    m_peeked = false;
+    return m_peeked_val;
+  }
+
+  void Remove() {
+    Next();
+  }
 private:
-	vector<int> m_nums;
-	int m_nums_size;
-	int m_pos;
+  void TryPeek() {
+    // if m_peeked == true, do nothing
+    while (!m_peeked && m_pos != m_end) {
+        if ( (*m_pos) % 5 == 0) {
+          m_peeked = true;
+          m_peeked_val = *m_pos;
+        }
+        ++ m_pos;
+    }
+  }
+  bool m_peeked;
+  int m_peeked_val;
+  ListIterator m_pos;
+  ListIterator m_end;
 };
 
-class ModFiveIterator : public Iterator {
-    int m_res;
-    bool m_peeked; // peeked next number or not, specificlly whether m_next_five has been updated or not
-public:
-	ModFiveIterator(const vector<int>& nums):
-	Iterator(nums),
-	m_res(-1),
-	m_peeked(false){
+void TestNextAndHasNext() {
+  std::list<int> values ;
 
-	}
-    int next() {
-        hasNext();
-        m_peeked = false;
-        return m_res;
-    }
-    
-    bool hasNext()  {
-	if(m_peeked) {
-		// already peeked the next valid number
-		return true;
-	} else {
-		// Let's peek the next number which%5==0
-    		while(Iterator::hasNext()) {
-			m_res = Iterator::next();
-			if (m_res % 5 ==0) {
-				m_peeked = true;
-				return true;
-			}
-		}
-		return false; // not found
-	}
-    }
-    
-};
+  for (int i = 0; i <= 20; ++i) {
+    values.push_back(i);
+  }
 
+  ModFiveIterator iter(values.begin(), values.end());
 
+  assert(iter.HasNext() == true);
+  assert(iter.Next() == 0);
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+  assert(iter.HasNext() == true);
+  assert(iter.Next() == 5);
 
-	vector<int> nums;
-	for (int i=1;i<=20;++i) {
-		nums.push_back(std::rand());
-	}
-	Iterator iter = Iterator(nums);
-	while(iter.hasNext()){
-		std::cout<< iter.next()<<',';
-	}
-	std::cout << std::endl;
-	ModFiveIterator five_iter = ModFiveIterator(nums);
-	while(five_iter.hasNext()){
-		std::cout<< five_iter.next()<<',';
-	}
-	std::cout << std::endl;
-	return 0;
+  assert(iter.HasNext() == true);
+  assert(iter.Next() == 10);
+
+  assert(iter.HasNext() == true);
+  assert(iter.Next() == 15);
+
+  assert(iter.HasNext() == true);
+  assert(iter.Next() == 20);
+
+  assert(iter.HasNext() == false);
+
+}
+void TestRemove() {
+  std::list<int> values ;
+
+  for (int i = 0; i <= 20; ++i) {
+    values.push_back(i);
+  }
+
+  ModFiveIterator iter(values.begin(), values.end());
+
+  assert(iter.HasNext() == true);
+  assert(iter.Next() == 0);
+
+  assert(iter.HasNext() == true);
+  assert(iter.Next() == 5);
+
+  iter.Remove();
+
+  iter.Remove();
+
+  assert(iter.HasNext() == true);
+  assert(iter.Next() == 20);
+
+  assert(iter.HasNext() == false);
+
 }
 
+
+int main() {
+  TestNextAndHasNext();
+  TestRemove();
+  return 0;
+}
