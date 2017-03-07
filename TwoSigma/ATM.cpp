89ace4a75ff89c1376c53f934ad4b8004303bedc
Yang Liu <yangliu1989@nyu.edu>
2017-03-06 21:12:10 -0500

    Update ATM.cpp

--- TwoSigma/ATM.cpp
+++ TwoSigma/ATM.cpp
@@ -1,3 +1,4 @@
+// Very good reference: http://www.math-cs.gordon.edu/courses/cs211/ATMExample/
 /***
 ATM oo design - 设计interface - 
 有基本的interface, 写implementation 
@@ -49,37 +50,70 @@ public:
   void Withdraw() {}
   void Inquery() {}
 private:
+  // States management
   void ChangeState(ATMStatePtr & state){m_state_ptr = state;}
   ATMStatePtr m_state_ptr; // maintain 
+  
+  // Component parts of the ATM
+  CardReaderPtr m_card_reader_ptr; // card reader
+  CashDispenserPtr m_cash_dispenser_ptr; // cash dispenser
+  CustomerConsolePtr m_customer_console_ptr; // customer console
+  LogPtr m_log_ptr; // Logger
+  OperatorPanelPtr m_operator_panel_ptr; // Operator Panel
+  ReceiptPrinterPtr m_receipt_printer_ptr; // Receipt Printer
 };
 
 class User { };
 
-// Account abstract class.
+// Account base class.
 // General implementation should be defined here.
 // While leave derived classes for account-type specific implementation.
 class Account {
 public:
-  void Deposit() {}
-  void Withdraw() {}
-  void Inquiry() {}
-  void Transfer() {}
+  virtual void Deposit() {}
+  virtual void Withdraw() {}
+  virtual void Inquiry() {}
+  virtual void Transfer() {}
 };
 
 class CheckingAccount : public Account {};
 class SavingAccount : public Account {};
 
-//Command pattern, memonto pattern may be suitable here depends on requirements.
+/***
+Transaction hierarchy.
+Command pattern, memonto pattern may be suitable here depends on requirements.
+***/
 class Transaction {
 public:
   virtual Execute() = 0;
   virtual void Undo() = 0; 
   virtual void Redo() = 0; 
+  virtual ~Transation() {};
 };
 
-class Withdraw : public Transaction {
+class Withdrawal : public Transaction {
 public:
-  Withdraw(AccountPtr account_ptr) {}
+  Withdrawal(AccountPtr account_ptr, double amount) 
+    : m_account_ptr(account_ptr), m_amount(amount), m_executed(false) {
+    
+  }
+  virtual void Execute() { 
+    if (!m_executed) { 
+      m_account_ptr->Withdraw(m_amount); 
+      m_executed = true;
+    }
+  }
+  virtual void Undo() { 
+    if (m_executed){
+      m_account_ptr->Withdraw(amount);
+      m_account_ptr->Deposit(amount);
+      m_executed = false;
+    }
+  }
+private:
+  bool m_executed;
+  AccountPtr m_account_ptr;
+  double m_amount;
 };
 
 // same for other transactions
