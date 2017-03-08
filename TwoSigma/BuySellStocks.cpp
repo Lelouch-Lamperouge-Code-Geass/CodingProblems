1854c848d6e05f56c74a7538643435e822ac52da
Yang Liu <yangliu1989@nyu.edu>
2017-03-07 20:56:04 -0500

    Update BuySellStocks.cpp

--- TwoSigma/BuySellStocks.cpp
+++ TwoSigma/BuySellStocks.cpp
@@ -1,3 +1,21 @@
+int maxProfit(vector<int>& prices) {
+      if (prices.empty()) return 0;
+      const std::size_t prices_size(prices.size());
+      std::vector<int> max_buy_one(prices_size,0), max_sell_one(prices_size,0);
+      std::vector<int> max_buy_two(prices_size,0), max_sell_two(prices_size,0);
+      max_buy_one[0] = max_buy_two[0] = -prices[0];
+
+      for (std::size_t i = 1; i < prices_size; ++i) {
+          max_buy_one[i] = std::max(max_buy_one[i-1], - prices[i]);
+          max_sell_one[i] = std::max(max_sell_one[i-1], max_buy_one[i] + prices[i]);
+          max_buy_two[i] = std::max(max_buy_two[i-1], max_sell_one[i] - prices[i]);
+          max_sell_two[i] = std::max(max_sell_two[i-1], max_buy_two[i] + prices[i]);
+      }
+
+      return max_sell_two[prices_size - 1];
+}
+
+
 
 int maxProfit(vector<int>& prices) {
   // These four variables records the maximum profit on that status.
